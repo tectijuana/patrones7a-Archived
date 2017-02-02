d31a4d2d5bd0f8b9a190c74d812e20fc65073a3a
omar-villegas <omar.villegas@tectijuana.edu.mx>
2017-02-02 13:21:28 -0800

    Add files via upload

---
+++ PracticasSwift/Omar-Villegas/Practica1.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Hello World
+//
+//  Created by Omar Villegas on 01/02/17.
+//  Copyright © 2017 Omar Villegas. All rights reserved.
+
+//  Omar Villegas Castillo 13211106
+//  1 de febrero del 2017
+// Practica 1
+
+
+print("Hola mundo!")
+
