9a48566ef96df3fd086b4dc7105a5390b5d3f7e6
AnthonyNeria <isc.anthony.neria@gmail.com>
2017-02-03 15:36:11 -0800

    Update Practica1.swift

--- PracticasSwift/AnthonyNeria/Practica1.swift
+++ PracticasSwift/AnthonyNeria/Practica1.swift
@@ -1,4 +1,4 @@
-//Neria Diaz Anthony Emmanuel 13211468
-//Patrones de diseño
-//Crear un programa que imprima "Hello world"
-print("Hello world")
+/*Neria Diaz Anthony Emmanuel 13211468
+Patrones de diseño
+Crear un programa que imprima "Hello world"*/
+print("Hello world")//imprime una cadena de caracteres dentro de parentesis entre comillas
