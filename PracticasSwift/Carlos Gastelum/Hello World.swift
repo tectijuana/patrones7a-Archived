001e43448e67419fa4bc135def5bc2e3c42338d3
carlosgastelum <carlosbiner@gmail.com>
2017-02-02 17:13:50 -0800

    Update Hello World.swift

--- PracticasSwift/Carlos Gastelum/Hello World.swift
+++ PracticasSwift/Carlos Gastelum/Hello World.swift
@@ -1,2 +1,17 @@
+//Carlos Gastelum Nieves 14210456
+
+
+
+//Patrones de diseño
+
+
+
+
+
+//Realizar  un programa en el lenguaje Swift´ que imprima el mensaje "Hello world" . 
+
+
+
+
 
 print("Hello world!")
