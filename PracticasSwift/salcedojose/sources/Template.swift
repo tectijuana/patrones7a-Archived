089564f9c645e6ed542780e4c0f8767d6f749c84
Salcedo Jose <josemasal@hotmail.com>
2017-02-02 16:00:31 -0800

    Tal vez borrar.

---
+++ PracticasSwift/salcedojose/sources/Template.swift
@@ -0,0 +1,9 @@
+/*
+Nombre del programa: Practica 0 Hola Mundo
+Autor: Salcedo Morales Jose Manuel
+Fecha de inicio: 2017-02-01
+Descripcion: Desplegar un simple hola mundo a la pantalla.
+*/
+
+// Importacion de librerias.
+import Foundation
