999106b5999873817884ddbfd36f6b99a4d38f0f
AnthonyNeria <isc.anthony.neria@gmail.com>
2017-02-10 18:46:48 -0800

    Add files via upload

---
+++ PracticasSwift/AnthonyNeria/Practica 4.swift
@@ -0,0 +1,13 @@
+/*Neria Diaz Anthony Emmanuel 13211468
+Patrones de diseño
+Capitulo 3, Ejercicio 24
+Practica 4: calcular el valor de el cateto restante de un triangulo isoceles introduciendo la hipotenusa y uno de los catetos.*/
+
+var H = 2 // hipotenusa
+var C = 3 //lado del triangulo
+
+var cateto = C //El triángulo isoceles tiene los catetos del mismo tamano.
+//se imprimen los resultados
+print ("Valor del lado del triangulo", C)
+print ("Valor de la hipotenusa", H)
+print("cateto restante:" , cateto)
