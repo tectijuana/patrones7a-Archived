ec313868b66aca3c512177c40de9a541ca9802c2
AnthonyNeria <isc.anthony.neria@gmail.com>
2017-02-10 19:03:03 -0800

    practicas

---
+++ PracticasSwift/AnthonyNeria/Practica 7.swift
@@ -0,0 +1,13 @@
+/*Neria Diaz Anthony Emmanuel 13211468
+Patrones de diseño
+Capitulo 3, Ejercicio 27
+Practica 7: introducir los lados de un tringulo rectangulo y calcular su area*/
+
+var lado1 = 58 
+var lado2 = 45
+var lado3 = 85
+var Perimetro = lado1 + lado2 + lado3 //perimetro suma de todos los lados.
+var area = perimetro * 2 //el area de un triangulo rectangulo es igual a 2 veces su perimetro
+//se imprimen los resultados
+print("El perimetro del triangulo es:" , Perimetro)
+print ("El area del triangulo es ", area)
