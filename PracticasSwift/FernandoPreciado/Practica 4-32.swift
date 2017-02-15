9ac7cb06f82a90e41ee024fbd91468fcd158aab2
fernandopreciado <fernando.preciado@tectijuana.edu.mx>
2017-02-14 22:06:47 -0800

    Add files via upload
    Practica 32 capitulo 4

---
+++ PracticasSwift/FernandoPreciado/Practica 4-32.swift
@@ -0,0 +1,32 @@
+// Alumno: Fernando Preciado Salman 
+// Numero de Control: 12211415
+// Patrones de diseño
+// Practica: 32 capitulo 4
+//  Realizar  un programa que muestre la operacion sin2(x) + cos2(x) = 1
+
+
+import Foundation
+    let s = sin(M_PI/1)
+	let c = cos(M_PI/1)
+	let p = pow(s, 2.0)
+	let q = pow(c, 2.0)
+	let z = p + q
+	print(z)
+ 	let s2 = sin(M_PI/5)
+	let c2 = cos(M_PI/5)
+	let p2 = pow(s2, 2.0)
+	let q2 = pow(c2, 2.0)
+	let z2 = p2 + q2
+	print(z)
+ 	let s3 = sin(M_PI/15)
+	let c3 = cos(M_PI/15)
+	let p3 = pow(s3, 2.0)
+	let q3 = pow(c3, 2.0)
+	let z3 = p3 + q3
+	print(z)
+ 	let s4 = sin(M_PI/25)
+	let c4 = cos(M_PI/25)
+	let p4 = pow(s4, 2.0)
+	let q4 = pow(c4, 2.0)
+	let z4 = p4 + q4
+	print(z)
