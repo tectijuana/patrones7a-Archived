1cbaf4de9774118aa0cffc9975d1746876b99758
fernandopreciado <fernando.preciado@tectijuana.edu.mx>
2017-02-14 23:14:45 -0800

    Update Practica 4-32.swift

--- PracticasSwift/FernandoPreciado/Practica 4-32.swift
+++ PracticasSwift/FernandoPreciado/Practica 4-32.swift
@@ -6,27 +6,14 @@
 
 
 import Foundation
-    let s = sin(M_PI/1)
-	let c = cos(M_PI/1)
+var i = 1.0
+for index in 1...20 {
+	i = (i + 1.0)
+    let s = sin(M_PI/i)
+	let c = cos(M_PI/i)
 	let p = pow(s, 2.0)
 	let q = pow(c, 2.0)
 	let z = p + q
 	print(z)
- 	let s2 = sin(M_PI/5)
-	let c2 = cos(M_PI/5)
-	let p2 = pow(s2, 2.0)
-	let q2 = pow(c2, 2.0)
-	let z2 = p2 + q2
-	print(z)
- 	let s3 = sin(M_PI/15)
-	let c3 = cos(M_PI/15)
-	let p3 = pow(s3, 2.0)
-	let q3 = pow(c3, 2.0)
-	let z3 = p3 + q3
-	print(z)
- 	let s4 = sin(M_PI/25)
-	let c4 = cos(M_PI/25)
-	let p4 = pow(s4, 2.0)
-	let q4 = pow(c4, 2.0)
-	let z4 = p4 + q4
-	print(z)
+}
+
