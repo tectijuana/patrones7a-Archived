38231965abc9b6d010fc1102bbcabd8f6f75773a
alejandro-espinoza <alesscovarrubias02@gmail.com>
2017-02-14 17:02:37 -0800

    Update Ejercicio6.swift

--- PracticasSwift/Alejandro-Espinoza/Ejercicio6.swift
+++ PracticasSwift/Alejandro-Espinoza/Ejercicio6.swift
@@ -1,29 +1,29 @@
 /* Espinoza Covarrubias Alejandro 13211465 */
-/* Patrones de diseÒo */
+/* Patrones de dise√±o */
 /* Capitulo 2 */
 /* Ejercicio 6 */
-/* Encontrar las raÌces cuadradas de los enteros del 9 al 25. Imprimir el entero y su raÌz cudradra */
+/* Encontrar las ra√≠ces cuadradas de los enteros del 9 al 25. Imprimir el entero y su ra√≠z cudradra */
 
-/* Importa librerÌa para utlizar funciones matem·ticas */
+/* Importa librer√≠a para utlizar funciones matem√°ticas */
 
 import Foundation
 
-/* DeclaraciÛn de variables */
+/* Declaraci√≥n de variables */
 
 var n = 9.0
 
-/* Ciclo while que realiza la raÌz cuadradra */
+/* Ciclo while que realiza la ra√≠z cuadradra */
 
 while n <= 25
 {
 	
-/* Calcular raÌz cuadradra */
+/* Calcular ra√≠z cuadradra */
 	
 var Cn = sqrt(n)
 	
 /* Imprime los resultados */
 
-print ("La raÌz cuadradra de \(n) es: \(Cn)")
+print ("La raiz cuadradra de \(n) es: \(Cn)")
 	
 /* Suma el valor de n una unidad */
 	
