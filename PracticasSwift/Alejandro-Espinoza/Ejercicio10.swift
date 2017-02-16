d0eb58c244844cb9aff7758f1ad5962b14fb37b2
alejandro-espinoza <alesscovarrubias02@gmail.com>
2017-02-15 16:53:54 -0800

    Update Ejercicio10.swift

--- PracticasSwift/Alejandro-Espinoza/Ejercicio10.swift
+++ PracticasSwift/Alejandro-Espinoza/Ejercicio10.swift
@@ -1,14 +1,14 @@
 /* Espinoza Covarrubias Alejandro 13211465 */
-/* Patrones de diseÒo */
+/* Patrones de dise√±o */
 /* Capitulo 2 */
 /* Ejercicio 10 */
 /* Imprimir una tabla de valores para y = a^(x) */
 
-/* Importa librerÌa para utlizar funciones matem·ticas */
+/* Importa librer√≠a para utlizar funciones matem√°ticas */
 
 import Foundation
 
-/* DeclaraciÛn de variables */
+/* Declaraci√≥n de variables */
 
 var x = 1.0
 var a = 2.0
@@ -28,7 +28,7 @@ while x <= 15
 	
 	print("\t\(x)\t|\t\(a)\t|\t\(y)\t")
 	
-	/* Suma el valor de n una unidad */
+	/* Suma al valor de x una unidad */
 	
 	x = x + 1
 	
