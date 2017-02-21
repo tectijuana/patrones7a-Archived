0033c51e9d8bdee85706af670af1cc8a2b5f06ee
carlosgastelum <carlosbiner@gmail.com>
2017-02-21 15:53:26 -0800

    Add files via upload

--- PracticasSwift/CarlosGastelum/programa.swift
+++ PracticasSwift/CarlosGastelum/programa.swift
@@ -1,26 +1,43 @@
 /*
-Nombre del programa: ...................	La clase de Ecología tiene cinco estuidantes que obtuvieron las siguientes notas en el examen final : 75,93,41,98,71. El profesor desea calcular el promedio de las notas. Escribur un programa para realizar el cálculo
- Creado por: Carlos Gastelum Nieves ..........  Gastelum Nieves Carlos 
+Nombre del programa: ...................	Introducir un entero N positivo. Encontrar la suma de los N enteros. Imprimir cada uno de los enteros y la suma.
+ Creado por: Carlos Gastelum Nieves ..........  
  No Control: .................................................14210456 
  Fecha ......................................................17-02-2017
-  
+ Practicas Swift del libro..........................................
 */
-	// Libreria
-	import Foundation
-	
-	var notas = [75.0,93,41,98,71]
-	
-	var suma = Double(0)
-	
-	//Realizamos el ciclo 
-	for i in 0...4
-	{
-	suma = suma + notas[i]
-	}
-	
-	//DIVISIÓN PARA OBTENER PROMEDIO
-	var promedio = suma / 5
-	
-	//imprimimos el promedio 
-	print("El promedio de notas a sido : \(promedio)")
-	
+
+
+
+// librerías
+import Foundation
+
+//declaramos nuestras constantes
+let n = 30
+//contador
+var I = 2
+
+//variable donde guardamos nuestra suma de los enteros ( N)
+var sum = 0
+
+// Aqui iremos guardando la cadena de la suma
+var txt = ""
+
+print("Suma de N valores")
+
+print("N = \(n)")
+
+//suma de los N valores
+while I<=n
+{
+sum = sum + I
+//Concatenacion de la suma
+if I!= n
+{txt += "\(I)+"}
+else
+{txt += "\(I)"}
+//incremento
+I = I+2
+}
+
+print("\(txt)=\(sum)")
+
