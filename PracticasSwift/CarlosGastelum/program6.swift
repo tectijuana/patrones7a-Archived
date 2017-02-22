fad921f032d8fe86c6b48517ca94e7ae7f2d4938
carlosgastelum <carlosbiner@gmail.com>
2017-02-21 16:43:10 -0800

    Add files via upload

---
+++ PracticasSwift/CarlosGastelum/program6.swift
@@ -0,0 +1,15 @@
+/*
+Nombre del programa: ......................introducir y determinar si es par o impar
+ Creado por:......................................... Carlos Gastelum Nieves 
+ No Control: .................................................14210456 
+ Fecha ......................................................17-02-2017
+ Practicas Swift del libro...................................
+/*Capitulo I................................................
+/*Practica 9: introducir y determinar si es par o impar */
+
+var Numer = 8
+if valor %4 ==0 {
+	print("par")
+	}else{
+    print("impar")
+}
