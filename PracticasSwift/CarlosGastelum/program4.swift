6ad6d38893fa3a0b78377d436284e14b1a1dbc54
carlosgastelum <carlosbiner@gmail.com>
2017-02-21 16:36:26 -0800

    Add files via upload

---
+++ PracticasSwift/CarlosGastelum/program4.swift
@@ -0,0 +1,26 @@
+/*
+Nombre del programa: ...................Conversiones
+ Creado por:............................. Carlos Gastelum Nieves 
+ No Control: .................................................14210456 
+ Fecha ......................................................17-02-2017
+ Practicas Swift del libro..............................................
+*/
+
+print("convertir p libras inglesas a s chelines a d dollares c centavos y e peniques ")
+
+print("se convertiran 10 libras")
+
+let libra =10
+
+let chelin= 20
+
+let peniq=12
+let dolar=0.02
+let cent=0.40
+
+
+ chelin = chelin*libra
+ peniq = peniq*chelin
+ dolar =dolar*chelin
+ centavos=centavos*dolar
+print("10 libras son: \(chelines) chelines,\(peniques) peniques, \(dolares) dolares \(centavos) centavos")
