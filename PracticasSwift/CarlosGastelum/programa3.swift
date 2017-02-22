15aef309259f2875faa57ccc0d7411806c472c72
carlosgastelum <carlosbiner@gmail.com>
2017-02-21 16:29:16 -0800

    Add files via upload

---
+++ PracticasSwift/CarlosGastelum/programa3.swift
@@ -0,0 +1,17 @@
+/*
+
+ Creado por:............................. Carlos Gastelum Nieves 
+ No Control: .................................................14210456 
+ Fecha ......................................................17-02-2017
+ Practicas Swift del libro..............................................
+
+Capitulo 3, Ejercicio 28
+Practica 8: Introducir X, la longitud de un lado de un
+triangulo equilatero y calcular su perimetro
+*/
+
+var Y = 78 // X es igual a un lado del triangulo equilatero
+var Perim = Y * 5 //El triángulo equilátero tiene los tres lados iguales, por lo que su perímetro será tres veces la longitud de uno de sus lados.
+//se imprimen los resultados
+print ("Valor que nos arroja triangulo es", Y)
+print("El perimetro del triangulo es:" , Perim)
