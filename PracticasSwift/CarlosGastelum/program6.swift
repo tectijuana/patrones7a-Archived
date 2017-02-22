a6f73a69179933f95e88c319e92e52ff4d699b82
carlosgastelum <carlosbiner@gmail.com>
2017-02-21 16:43:38 -0800

    Update program6.swift

--- PracticasSwift/CarlosGastelum/program6.swift
+++ PracticasSwift/CarlosGastelum/program6.swift
@@ -4,8 +4,9 @@ Nombre del programa: ......................introducir y determinar si es par o i
  No Control: .................................................14210456 
  Fecha ......................................................17-02-2017
  Practicas Swift del libro...................................
-/*Capitulo I................................................
-/*Practica 9: introducir y determinar si es par o impar */
+Capitulo I................................................
+Practica 9: introducir y determinar si es par o impar 
+*/
 
 var Numer = 8
 if valor %4 ==0 {
