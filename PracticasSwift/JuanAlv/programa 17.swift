09e05f930aa20f94cf2587162d2ec8cf1b6fd6f8
AlvRdzJuan <Juan.alvarado16@tectijuana.edu.mx>
2017-02-28 19:22:52 -0800

    Update programa 17.swift

--- PracticasSwift/JuanAlv/programa 17.swift
+++ PracticasSwift/JuanAlv/programa 17.swift
@@ -1,4 +1,14 @@
-//17. calcular e imprimir la suma de los enteros del 1 al 20
+
+//Title: Problema 17	   		         Filename: programa 17.swift
+
+//Author: Alvarado Rodriguez Juan Manuel          Date: 22 - Feb - 2017
+
+//Description: calcular e imprimir la suma de los enteros del 1 al 20
+
+//Input: -
+
+//Output: suma de enteros
+
 var sum:Int=0
 for i in 1...20{
 	sum = sum + i
