7b2c6cf7f7f7b2e50f41e227efe3ff7dbd38ad0a
AlvRdzJuan <Juan.alvarado16@tectijuana.edu.mx>
2017-02-28 19:31:46 -0800

    Update programa 14.swift

--- PracticasSwift/JuanAlv/programa 14.swift
+++ PracticasSwift/JuanAlv/programa 14.swift
@@ -1,14 +1,14 @@
 
-//Title: Problema 12	   		         Filename: programa 12.swift
+//Title: Problema 14	   		         Filename: programa 14.swift
 
 //Author: Alvarado Rodriguez Juan Manuel          Date: 22 - Feb - 2017
 
-//Description: encontrar el entero positivo mayor en una lista de quince enteros, algunos de los cuales son impares
+//Description: Determinar cual cantidad es mayor: 3^75 o 2^100
 
-//Input: 15 numeros
+//Input: -
 
-//Output: numero mayor
-//14. Determinar cual cantidad es mayor: 3^75 o 2^100
+//Output: mayor
+
 if pow(3,75)==pow(2,100){
 	print("3^75 es igual que 2^100")
 	}
