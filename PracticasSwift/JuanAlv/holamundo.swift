eec836e5d7cb74717ae4dc171533817e7759f495
AlvRdzJuan <Juan.alvarado16@tectijuana.edu.mx>
2017-02-28 19:41:57 -0800

    Update holamundo.swift

--- PracticasSwift/JuanAlv/holamundo.swift
+++ PracticasSwift/JuanAlv/holamundo.swift
@@ -1,1 +1,10 @@
+//Title: Hola Mundo 	   		         Filename: holamundo.swift
+
+//Author: Alvarado Rodriguez Juan Manuel          Date: 13 - Feb - 2017
+
+//Description: imprimir "hello world"
+
+//Input: -
+
+//Output: hello world
 print("hello world")
