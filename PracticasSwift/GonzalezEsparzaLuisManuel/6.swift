239eddc4687a9a598c00e654111c7ca39c753f04
iscgonzalezluis <isc.gonzalezluis@gmail.com>
2017-03-02 13:03:38 -0800

    subiendo los 10 programas swift por separado

---
+++ PracticasSwift/GonzalezEsparzaLuisManuel/6.swift
@@ -0,0 +1,31 @@
+//  Write some awesome Swift code, or import libraries like "Foundation",
+//  "Dispatch", or "Glibc"
+// link de donde los hice en linea http://swift.sandbox.bluemix.net/#/repl/58b7d995cb7993767588ce10
+import Glibc
+
+print("6) ");
+print("9.18 Una compania juguetera reduce el precio de sus coches y camiones metalicos en un 12%. Calcular la reduccion promedio en precio de los coches y camiones que originalmente costaban $8.95,$12.50,$5.50,$14.25,$9.50,$7.50,$10.00 y $3.20 ");
+var coche1: Double
+var coche2: Double
+var coche3: Double
+var coche4: Double
+var coche5: Double
+var coche6: Double
+var coche7: Double
+var coche8: Double
+coche1=8.95;
+coche2=12.50;
+coche3=5.50;
+coche4=14.25;
+coche5=9.50;
+coche6=7.50;
+coche7=10.00;
+coche8=3.20;
+print("coche1:$",coche1*0.88)
+print("coche2:$",coche2*0.88)
+print("coche3:$",coche3*0.88)
+print("coche4:$",coche4*0.88)
+print("coche5:$",coche5*0.88)
+print("coche6:$",coche6*0.88)
+print("coche7:$",coche7*0.88)
+print("coche8:$",coche8*0.88)
