2c4f4319f166f93fdbd2a53ba0c46d688e1d9d43
chipres <ana.chipres16@tectijuana.edu.mx>
2017-03-02 16:11:42 -0800

    programas del libro

---
+++ PracticasSwift/ChipresAna/Programa5.swift
@@ -0,0 +1,30 @@
+/*
+Title: Ejercicio 55 Lección 1                      
+
+Autor: Chipres Castellanos Ana Maria 
+
+Description: Suponer que toda la gente duerme alrededor de 1/3 del tiempo (8 horas de 24). Calcular cuantas horas ha dormido una persona durante su vida. Un año tiene 365 dias (no considerando años bisiestos)
+
+Date: 1-03-2017
+
+*/
+
+// Importacion de libreria
+import Foundation
+
+
+//imprimir años de vida
+print ("Calcular en 23 años de vida ")
+
+// Declaracion de constantes
+let horas = 8
+// Declaracion de constantes
+let ano = 365
+ //multiplicacion de variables
+var horasxano = (horas * ano)
+// multiplicacion de horas por años de vida
+var horasvida = (horasxano * 23)
+
+
+//imprimir las horas en 23 años de vida
+print (horasvida)
