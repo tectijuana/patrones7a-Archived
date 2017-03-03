91a8085c16292d9fc6684e309c8994a435dcc51d
AshleyOrtega <ashley.ortega@tectijuana.edu.mx>
2017-03-03 02:00:20 -0800

    Practica Hola Mundo

---
+++ PracticasSwift/OrtegaAshley/HolaMundo.swift
@@ -0,0 +1,7 @@
+//  Instituto Tecnologico de Tijuana",
+//  "Semestre Enero-Junio 2017",
+//  "Patrones de Diseño",
+//  "Ortega Rodriguez Ashley Karina 12211430",
+//  "Imprimir Hola Mundo en Swift"
+
+print("Hello world!"
