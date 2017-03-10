678d21a89ae8213ebaaf85a160696390cccdc966
Lab-PC <Lab-PC>
2017-03-09 16:34:53 -0800

    agregando

---
+++ PracticasSwift/CastroRicardo/Ejercicio 1.44.swift
@@ -0,0 +1,13 @@
+
+//Castro Mariel Ricardo Daniel 13211403
+// Patrones de diseño 
+// Capitulo 1 
+// Ejercicio
+//1.44
+//tapete
+
+let tapete:Double = 9
+var tapete2:Double = 0
+tapete2 = tapete / 0.9144
+
+print("$9 per yard = $ \(tapete2) per meter")
