e8c9e1b65c025f2ac83097255ae724485a079d14
ulisesmendez <tec.adriantf@gmail.com>
2017-03-13 10:17:50 -0700

    corrigiendo practicas

--- PracticasSwift/SebastianAleman/Practica2.swift
+++ PracticasSwift/SebastianAleman/Practica2.swift
@@ -4,7 +4,8 @@
 /*Capitulo I*/
 /*Practica 2: imprimir los enteros impares del 7 al 51 */
 
-var suma=0
-for var multiplicador = 7;  multiplicador <51 ++ multiplicador
-suma = multiplicador 
-print(suma)
+let EnterosImpares = ["7", "9", "11", "13", "15", "17", "19", "21", "23", "25", "27", "29", "31", "33", "55", "37", "39", "41","43", "45", "47", "49", "51",]
+let count = EnterosImpares.count
+for i in 0..<count {
+    print(" \(EnterosImpares[i])")
+}
