e2d2bd6b0b133ecaa3d1458ddf668109e21be7d7
AdrianTorres <tec.adriantf@gmail.com>
2017-03-13 09:28:49 -0700

    corrigiendo

--- PracticasSwift/AdrianTorres /HolaMundo.swift
+++
@@ -1,9 +0,0 @@
-/*Alumno: Ivan Adrian Torres Flores*/
-/* Numero de Control: 13210388*/
-/*Patrones de diseño*/
-/*Practica: 1 Imprimir mensaje HOLA MUNDO*/
-
-
-var saludo = "hola mundo"
-
-print(saludo)
