84457a41e76ce1bdaafbd56c8c634737357ec2d3
ulisesmendez <tec.adriantf@gmail.com>
2017-03-13 12:17:45 -0700

    Corrigiendo Practica 10

--- PracticasSwift/SebastianAleman/Practica10.swift
+++ PracticasSwift/SebastianAleman/Practica10.swift
@@ -5,11 +5,9 @@
 /*Practica 10: Escribir un programa que acepte 25 enteros positivos como datos y 
 describir cada uno como "impar" o "par"  */
 
-let numeros = 25
-
-if numero %2{
-	print ("es par")
-
-} else{
-	print ("es impar")
+let DescripcionNumeros = ["1 Impar","2 Par","3 Impar","4 Par","5 Impar","6 Par","7 Impar","8 Par","9 Impar","10 Par","11 Impar","12 Par","13 Impar","14 Par","15 Impar","16 Par","17 Impar","18 Par ", "19 Impar","20 Par","21 Impar","22 Par","23 Impar","24 Par","25 Impar"]
+let count = DescripcionNumeros.count
+for i in 0..<count {
+    print(" \(DescripcionNumeros[i])")
 }
+
