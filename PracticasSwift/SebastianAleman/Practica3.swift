36cf2a19c0c827af23a30576fedebd0a5b17394a
ulisesmendez <tec.adriantf@gmail.com>
2017-03-13 10:33:10 -0700

    corrigiendo practica 3

--- PracticasSwift/SebastianAleman/Practica3.swift
+++ PracticasSwift/SebastianAleman/Practica3.swift
@@ -3,5 +3,8 @@
 /*Patrones de diseño*/
 /*Practica 3: Imprimir numeros enteros pares del 2 al 48*/
 
-let numeros  = Array(1...24) 
-numeros.map(multiplica2)
+let EnterosPares = ["2","4","6","8","10","12","14","16","18","20","22","24","26","28","30","32","34","36","38","40","42","44","46","48"]
+let count = EnterosPares.count
+for i in 0..<count {
+    print(" \(EnterosPares[i])")
+}
