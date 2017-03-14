5c242ab5b41aacc9fee8c98e454205a5fe640300
ivanatorresf <tec.adriantf@gmail.com>
2017-03-14 09:36:16 -0700

    Update problema52.swift
    corrigiendo

--- PracticasSwift/IvanTorres/problemas-prog-swift/problema52.swift
+++ PracticasSwift/IvanTorres/problemas-prog-swift/problema52.swift
@@ -2,7 +2,9 @@
 /* Numero de Control: 13210388*/
 /*Patrones de diseño*/
 
-/*Los señores X y Y vana atener un duelo, en el que cada uno dsispara despues del otro. X, atina, en promedio, uno de cada dos disparos, Y,  solo uno en cada tres disparos. Como todo un caballero, X accede a que Y dispare primero. Simular el duelo y tratar de determinar quien tiene la mas alta probabilidad de sobrevivir.*/
+/*Los señores X y Y vana atener un duelo, en el que cada uno dsispara despues del otro. X, atina, en promedio, 
+uno de cada dos disparos, Y, solo uno en cada tres disparos. Como todo un caballero, X accede a que Y dispare primero. 
+Simular el duelo y tratar de determinar quien tiene la mas alta probabilidad de sobrevivir.*/
 
 /*
 X  1/2    sumar 1 cada 2 disparos
@@ -11,54 +13,16 @@ Y  1/3    sumar 1 cada 3 disparos
 quien tiene mas probabilidades de sobrevivir?*/
 
 
-if(nu1>nu2&&nu1>nu3)
-{
+let tiros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
+var puntajeIndividual = 0
 
-if(nu2>nu3)
-{
+for puntaje in tiros {
 
-print(nu1!,nu2!,nu3!)
-
+    if puntaje > 50 {
+        puntajeIndividual += 3        
+    } else {
+        puntajeIndividual += 1
+    }
 }
-else
-{
-print(nu1!,nu3!,nu2!)
-}
-
-
-}
-
-else if(nu2>nu1&&nu2>nu3)
-{
-
-if(nu1>nu3)
-{
-print(nu2!,nu1!,nu3!)
-
-}
-else
-{
-
-print(nu2!,nu3!,nu1!)
-}
-
-}
-else if(nu3>nu1&&nu3>nu2)
-{
-
-
-if(nu1>nu2)
-{
-
-print(nu3!,nu1!,nu2!)
-}
-else{
-
-print(nu3!,nu2!,nu1!)
-
-}
-
-
-
-}
+print(puntajeIndividual)
